f60931a53a6771b45e3341ddb7c60ec659339e22
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-07-23 09:46:07 +0000

    Deploy kNowSeeds/knowseeds.github.io to github.com/kNowSeeds/knowseeds.github.io.git:gh-pages

---
+++ precache-manifest.429c214cc14020bc86a5cc5a80a04f0d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b67b6e743cfd4022e5ba",
+    "url": "/css/app.02bf5b89.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0ef0f4ee36ba9bcc9b95289ced4baf9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b2b5a41d346ce6f96d0",
+    "url": "/js/about.2b8983e6.js"
+  },
+  {
+    "revision": "b67b6e743cfd4022e5ba",
+    "url": "/js/app.987583de.js"
+  },
+  {
+    "revision": "ce3d5594ceff03bbe99e",
+    "url": "/js/chunk-vendors.6d2cd856.js"
+  },
+  {
+    "revision": "69b649f3f7f4f8f6e1b3afed797a8459",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
